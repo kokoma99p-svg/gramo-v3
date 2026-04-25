f3f1c78d69ae33212511ce13cc75e4c3b0678701
kokoma99p-svg <kokoma99p@gmail.com>
2026-04-25 22:32:03 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>GRAMO is live 🚀</h1>
+      <p>Your Vercel deployment is now fixed.</p>
+    </div>
+  );
+}
